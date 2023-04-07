4e1527fc4d5aec000967bc42fe3252af85f3006f
Ed Engelking <ed@engelking.me>
2023-04-07 17:21:35 +0000

    Fixed root volume size in Terraform. Fixed bugs in Bastion script

--- scripts/01-bastion.sh
+++ scripts/01-bastion.sh
@@ -4,7 +4,7 @@
 if ! multipass list | grep -q "bastion"; then
   echo "Creating Bastion Server: bastion"
   multipass launch -n bastion -c 2 -m 2G -d 10G
-  multipass transfer base.sh lb-cp:/tmp/base.sh
+  multipass transfer base.sh bastion:/tmp/base.sh
   multipass exec bastion -- chmod +x /tmp/base.sh
   multipass exec bastion -- sudo bash /tmp/base.sh
 fi
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,54 @@
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
+  access_key              = var.aws_access_key
+  secret_key              = var.aws_secret_key
+  region                  = var.region
 }
 
 resource "aws_security_group" "ssh_access" {
-  name        = "baremetal_ssh_access"
-  description = "Allow SSH inbound traffic for Baremetal server nodes"
+  name                    = "baremetal_ssh_access"
+  description             = "Allow SSH inbound traffic for Baremetal server nodes"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port             = 22
+    to_port               = 22
+    protocol              = "tcp"
+    cidr_blocks           = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port             = 0
+    to_port               = 0
+    protocol              = "-1"
+    cidr_blocks           = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Baremetal Instance"
+    Name                  = "Baremetal Instance"
   }
 }
 
 resource "aws_instance" "baremetal_instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.ssh_key
-  count         = var.instance_count
-  availability_zone = var.zone
+  ami                     = var.ami_id
+  instance_type           = var.instance_type
+  key_name                = var.ssh_key
+  count                   = var.instance_count
+  availability_zone       = var.zone
 
   vpc_security_group_ids = [aws_security_group.ssh_access.id]
 
+  root_block_device {
+    volume_size           = "250"
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
+
   tags = {
-    Name = "Baremetal Instance"
+    Name                  = "Baremetal Instance"
   }
 }
 
 resource "local_file" "inventory" {
-  content  = join("\n", aws_instance.baremetal_instance.*.public_ip)
-  filename = "../ansible/inventory.txt"
-  depends_on = [aws_instance.baremetal_instance]
+  content                 = join("\n", aws_instance.baremetal_instance.*.public_ip)
+  filename                = "../ansible/inventory.txt"
+  depends_on              = [aws_instance.baremetal_instance]
 }
