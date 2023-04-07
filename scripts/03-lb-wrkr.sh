2db61ed6abb3d3cbde9b42569e6e409dd2843c26
Ed Engelking <ed@engelking.me>
2023-04-07 16:59:02 +0000

    Fixed bug in lb-wrkr script

--- scripts/03-lb-wrkr.sh
+++ scripts/03-lb-wrkr.sh
@@ -4,7 +4,7 @@
 if ! multipass list | grep -q "lb-wrkr"; then
   echo "Creating Worker Load Balancer: lb-wrkr"
   multipass launch -n lb-wrkr -c 2 -m 2G -d 10G
-  multipass transfer base.sh lb-cp:/tmp/base.sh
+  multipass transfer base.sh lb-wrkr:/tmp/base.sh
   multipass exec lb-wrkr -- chmod +x /tmp/base.sh
   multipass exec lb-wrkr -- sudo bash /tmp/base.sh
 fi
