23f25a53062582a4aed129989d7352908837cc88
Ed Engelking <ed@engelking.me>
2023-04-07 19:40:29 +0000

    Improve documentation

--- scripts/06-config.sh
+++ scripts/06-config.sh
@@ -15,71 +15,233 @@ done
 ## Show the RKE2 config.yaml
 cat << EOF > rke2-install.txt
 
-## -----
-## RKE2 Installation Instructions
-## -----
-## Install the rke2-server service
+1. Copy the contents of this file to your local machine.
+2. Open three terminals.
+3. Follow the instructions below.
+
+## =====
+## Control-Plane Load Balancer, terminal #1
+## =====
+## We will be using the CP LB to view the control-plane nodes coming online
+## We will also be able to view ingress traffic through the LB to the nodes
+
+
+1. From terminal #1, login to the Control-Place Load Balancer
+-----
+sudo multipass shell lb-cp
+
+2. Run the command to view the CLI UI for HAproxy.
+-----
+sudo hatop -s /var/run/haproxy/haproxy.sock
+
+
+
+## =====
+## Bastion Node, terminal #2
+## =====
+## We will be using the Bastion node as the centralized point to manage all nodes.
+## The bastion prevents exposure of the HA RKE2 cluster to the world.
+
+1. From terminal #2, login to the Bastion node
+-----
+sudo multipass shell bastion
+
+
+2. SSH to the CP-1 Node from the Bastion Node
+-----
+ssh cp-1
+
+
+3. Install RKE2 on CP-1 as the primary control-plane server
+-----
 curl -sfL https://get.rke2.io | sudo sh -
+
+
+4. Create the directory to store the configuration for RKE2
+-----
 sudo mkdir -p /etc/rancher/rke2/
+
+
+3. Edit the config.yaml file
+-----
 sudo vim /etc/rancher/rke2/config.yaml
 
-## Install rke2-agent service
-curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" sudo sh -
-sudo mkdir -p /etc/rancher/rke2/
-sudo vim /etc/rancher/rke2/config.yaml
 
-## Systemctl Server commands
+4. Paste the following into the /etc/rancher/rke2/config.yaml file
+-----
+token: QMXrt0w8BWFO5Z1D0zLm5VKq5HF74Yl8EE9IR72YUqUtaeW4Xix1tCgdzJU0meE5
+write-kubeconfig-mode: "0644"
+node-taint:
+  - "CriticalAddonsOnly=true:NoExecute"
+tls-san:
+  - demo.ablabs.io
+  - ${node_ips[cp-1]}
+  - ${node_ips[cp-2]}
+  - ${node_ips[cp-3]}
+  - ${node_ips[lb-cp]}
+  - ${node_ips[lb-wrkr]}
+
+
+5. Save the configuration
+-----
+:wq (ENTER)
+
+
+6. Reload the Systemctl daemon and enable the rke2-server. It will take a few moments to come online.
+-----
 sudo systemctl daemon-reload
 sudo systemctl enable --now rke2-server
 
-## Systemctl Agent commands
+
+7. Verify the cluster has come online
+-----
+sudo /var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml get nodes
+
+
+## =====
+## Control-Plane Load Balancer, terminal #1
+## =====
+## Watch the interface in the HAproxy load balancer UI
+## First to come online will be controlplane-9345, main1
+## Next to come online will be controlplane-6443, main1
+## When both of these show UP, you can move on to the next step
+
+
+
+## =====
+## Bastion Node, terminal #2
+## =====
+## Once the first control-plane node is online, we can add additional nodes to the cluster
+## Using the Bastion node, we will switch from cp-1 to cp-2 and so forth
+
+1. From terminal #2, exit the cp-1 server back to the Bastion node command line
+-----
+exit
+
+2. SSH to the CP-2 Node from the Bastion Node
+-----
+ssh cp-2
+
+
+3. Install RKE2 on CP-2 as the first secondary control-plane server
+-----
+curl -sfL https://get.rke2.io | sudo sh -
+
+
+4. Create the directory to store the configuration for RKE2
+-----
+sudo mkdir -p /etc/rancher/rke2/
+
+
+3. Edit the config.yaml file
+-----
+sudo vim /etc/rancher/rke2/config.yaml
+
+
+4. Paste the following into the /etc/rancher/rke2/config.yaml file. Note the addition of the "server" parameter.
+-----
+server: https://${node_ips[lb-cp]}:9345
+token: QMXrt0w8BWFO5Z1D0zLm5VKq5HF74Yl8EE9IR72YUqUtaeW4Xix1tCgdzJU0meE5
+write-kubeconfig-mode: "0644"
+node-taint:
+  - "CriticalAddonsOnly=true:NoExecute"
+tls-san:
+  - demo.ablabs.io
+  - ${node_ips[cp-1]}
+  - ${node_ips[cp-2]}
+  - ${node_ips[cp-3]}
+  - ${node_ips[lb-cp]}
+  - ${node_ips[lb-wrkr]}
+
+
+5. Save the configuration
+-----
+:wq (ENTER)
+
+
+6. Reload the Systemctl daemon and enable the rke2-server. It will take a few moments to come online.
+-----
+sudo systemctl daemon-reload
+sudo systemctl enable --now rke2-server
+
+
+7. Verify the cluster has come online
+-----
+sudo /var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml get nodes
+
+
+8. Repeat steps 1-7 for the remaining CP-3 server node, changing cp-2 to cp-3.
+-----
+
+
+## =====
+## Control-Plane Load Balancer, terminal #1
+## =====
+## Watch the interface in the HAproxy load balancer UI
+## First to come online will be controlplane-9345, main3
+## Next to come online will be controlplane-6443, main3
+## When both of these show UP, you will have successfully deployed the HA control-plane
+
+
+
+## =====
+## Bastion Node, terminal #2
+## =====
+## Now, add the worker nodes to the cluster. There are 3 worker nodes: wrkr-1, wrkr-2, wrkr-3
+## Repeat the steps below for each of the worker nodes to bring them online.
+
+1. From terminal #2, exit the cp-3 server back to the Bastion node command line
+-----
+exit
+
+2. SSH to the WRKR-1 Node from the Bastion Node
+-----
+ssh wrkr-1
+
+
+3. Install RKE2 on WRKR-1. Note the command is different from earlier.
+-----
+curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" sudo sh -
+
+
+4. Create the directory to store the configuration for RKE2
+-----
+sudo mkdir -p /etc/rancher/rke2/
+
+
+3. Edit the config.yaml file
+-----
+sudo vim /etc/rancher/rke2/config.yaml
+
+
+4. Paste the following into the /etc/rancher/rke2/config.yaml file. Note the difference in the config.
+-----
+server: https://${node_ips[lb-cp]}:9345
+token: QMXrt0w8BWFO5Z1D0zLm5VKq5HF74Yl8EE9IR72YUqUtaeW4Xix1tCgdzJU0meE5
+
+
+5. Save the configuration
+-----
+:wq (ENTER)
+
+
+6. Reload the Systemctl daemon and enable the rke2-agent. It will take a few moments to come online.
+-----
 sudo systemctl daemon-reload
 sudo systemctl enable --now rke2-agent
 
-## -----
-## Primary Server config.yaml
-## -----
-token: QMXrt0w8BWFO5Z1D0zLm5VKq5HF74Yl8EE9IR72YUqUtaeW4Xix1tCgdzJU0meE5
-write-kubeconfig-mode: "0644"
-node-taint:
-  - "CriticalAddonsOnly=true:NoExecute"
-tls-san:
-  - demo.ablabs.io
-  - ${node_ips[cp-1]}
-  - ${node_ips[cp-2]}
-  - ${node_ips[cp-3]}
-  - ${node_ips[lb-cp]}
-  - ${node_ips[lb-wrkr]}
 
-## -----
-## Secondary Server config.yaml
-## -----
-server: https://${node_ips[lb-cp]}:9345
-token: QMXrt0w8BWFO5Z1D0zLm5VKq5HF74Yl8EE9IR72YUqUtaeW4Xix1tCgdzJU0meE5
-write-kubeconfig-mode: "0644"
-node-taint:
-  - "CriticalAddonsOnly=true:NoExecute"
-tls-san:
-  - demo.ablabs.io
-  - ${node_ips[cp-1]}
-  - ${node_ips[cp-2]}
-  - ${node_ips[cp-3]}
-  - ${node_ips[lb-cp]}
-  - ${node_ips[lb-wrkr]}
+7. Verify the worker node has come online
+-----
+exit
+ssh cp-1
+sudo /var/lib/rancher/rke2/bin/kubectl --kubeconfig /etc/rancher/rke2/rke2.yaml get nodes
 
-## -----
-## Agent config.yaml
-## -----
-server: https://${node_ips[lb-cp]}:9345
-token: QMXrt0w8BWFO5Z1D0zLm5VKq5HF74Yl8EE9IR72YUqUtaeW4Xix1tCgdzJU0meE5
-write-kubeconfig-mode: "0644"
-node-taint:
-  - "CriticalAddonsOnly=true:NoExecute"
-tls-san:
-  - demo.ablabs.io
-  - ${node_ips[cp-1]}
-  - ${node_ips[lb-cp]}
-  - ${node_ips[lb-wrkr]}
+
+8. Repeat steps 1-7 for the remaining worker nodes.
+-----
+
+
 EOF
 
 ## Create the Control-Plane HAProxy config file
