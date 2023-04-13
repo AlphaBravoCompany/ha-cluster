8070599abae851455001805bb4ba74230119f626
Mike 'MJ' Johnson <mikejohnsonit@gmail.com>
2023-04-13 14:30:13 +0000

    fix 09 script

--- scripts/09-bastion-config.sh
+++ scripts/09-bastion-config.sh
@@ -4,8 +4,9 @@
 if ! multipass list | grep -q "bastion"; then
   if ! multipass list | grep -q "bastion"; then
     echo "Configuring Bastion Server: bastion"
-    multipass transfer /var/snap/multipass/common/data/multipassd/ssh-keys/id_rsa bastion:/home/ubuntu/.ssh/id_rsa
-    multipass transfer /usr/local/bin/kubectl bastion:/home/ubuntu/kubectl
-    multipass exec bastion -- chmod +x /tmp/bastion.sh
-    multipass exec bastion -- sudo bash /tmp/bastion.sh
+    sudo multipass transfer /var/snap/multipass/common/data/multipassd/ssh-keys/id_rsa bastion:/home/ubuntu/.ssh/id_rsa
+    sudo multipass transfer /usr/local/bin/kubectl bastion:/home/ubuntu/kubectl
+    sudo multipass exec bastion -- chmod +x /tmp/bastion.sh
+    sudo multipass exec bastion -- sudo bash /tmp/bastion.sh
+  fi
 fi
